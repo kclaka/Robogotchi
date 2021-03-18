865bc8519a6a078bb5e90451666ff65f4ac0f7dd
Kenny <kennyigbechi@gmail.com>
2021-03-17 17:21:46 -0700

    FINISHED PROJECT

--- Robogotchi/task/robogotchi.py
+++ Robogotchi/task/robogotchi.py
@@ -119,11 +119,11 @@ def playRPS():
 
 
 def startConsole():
-    gameOptions = ["Rock-paper-scissors", "Numbers"]
+    gameOptions = ["rock-paper-scissors", "numbers"]
 
     while True:
         choose_game = input("Which game would you like to play?")
-        if not (choose_game in gameOptions):
+        if not (choose_game.lower() in gameOptions):
             print("Please choose a valid option: Numbers or Rock-paper-scissors?")
             continue
         elif choose_game.lower() == gameOptions[0].lower():
@@ -134,15 +134,51 @@ def startConsole():
             break
 
 
-def getBattery():
-    pass
+def unfortunateEvent(name, rust):
+    message = ["sprinkler", "nothing", "nothing", "nothing", "puddle", "sprinkler", "pool", "nothing", "pool", "nothing", "nothing", "nothing"]
+
+    choice = message[random.randint(0, len(message) - 1)]
+
+    if choice == "puddle":
+        rust += 10
+        print(f"Oh no, {name} stepped into a puddle!")
+    if choice == "sprinkler":
+        rust += 30
+        print(f"Oh, {name} encountered a sprinkler!")
+    if choice == "pool":
+        print(f"Guess what! {name} fell into the pool!")
+        rust += 50
+
+    return rust
 
 
-def printRobotInfo(name, battery_level, over_heat, skills, boredom):
-    print(f"{name}'s stats are: the battery is {battery_level},")
+def requestRecharge(name, battery, choice):
+    if isBatteryEmpty(battery) and not choice == "recharge":
+        print(f"The level of the battery is 0, {name} needs recharging!")
+        return True
+    return False
+
+
+def isBatteryEmpty(battery):
+    if battery <= 0:
+        return True
+
+    return False
+
+
+def isFunEmpty(name, boredom, choice):
+    if boredom == 100 and not choice == "play":
+        print(f"{name} is too bored! {name} needs to have fun!")
+        return True
+    return False
+
+
+def printRobotInfo(name, battery_level, over_heat, skills, boredom, rust):
+    print(f"{name}'s stats are:\nthe battery is {battery_level},")
     print(f"overheat is {over_heat},")
     print(f"skill level is {skills},")
-    print(f"boredom is {boredom}.")
+    print(f"boredom is {boredom},")
+    print(f"rust is {rust}.")
 
 
 def setName():
@@ -155,24 +191,31 @@ def controlRobot():
 
     battery_level = 100
     over_heat = 0
-    skills = 0
+    skill = 0
     boredom = 0
+    rust = 0
 
-    choices = ["exit", "info", "recharge", "sleep", "play", "exit"]
+    choices = ["exit", "info", "recharge", "sleep", "play", "learn", "oil", "work"]
 
     while True:
         print(f"Available interactions with {name}:")
         print(
             "exit - Exit\n"
             "info - Check the vitals\n"
+            "work - Work\n"
+            "play - Play\n"
+            "oil - Oil\n"
             "recharge - Recharge\n"
             "sleep - Sleep mode\n"
-            "play - Play"
+            "learn - Learn skills"
+
         )
-        if over_heat == 100:
+        if over_heat >= 100:
             print(f"The level of overheat reached 100, {name} has blown up! Game over. Try again?")
             break
-
+        if rust >= 100:
+            print(f"{name} is too rusty! Game over. Try again?")
+            break
         choice = input("Choose:")
 
         if not choice in choices:
@@ -181,8 +224,14 @@ def controlRobot():
         elif choice == choices[0]:
             break
         elif choice == choices[1]:
-            printRobotInfo(name, battery_level, over_heat, skills, boredom)
+            printRobotInfo(name, battery_level, over_heat, skill, boredom, rust)
+            continue
+        elif requestRecharge(name, battery_level, choice):
+            continue
+        elif isFunEmpty(name, boredom, choice):
+            continue
         elif choice == choices[4]:
+            rust = unfortunateEvent(name, rust)
             startConsole()
             prev_boredom = boredom
             prev_overheat = over_heat
@@ -195,6 +244,7 @@ def controlRobot():
             print(f"{name}'s level of overheat was {prev_overheat}. Now it is {over_heat}.")
             if boredom == 0:
                 print(f"{name} is in a great mood!")
+            continue
         elif choice == choices[3]:
             prev_value = over_heat
             if over_heat == 0:
@@ -225,10 +275,66 @@ def controlRobot():
                     f"{name}'s level of boredom was {temp_boredom}. Now it is {boredom}."
                     f"{name} is recharged!"
                 )
-
         elif choice == choices[5]:
+            prev_skill = skill
+            if skill >= 100:
+                print(f"There's nothing for {name} to learn!")
+            else:
+                skill += 10
+            print(f"{name}'s level of skill was {prev_skill}. Now it is {skill}.")
+            prev_heat = over_heat
+            over_heat += 10
+            print(f"{name}'s level of overheat was {prev_heat}. Now it is {over_heat}.")
+            prev_batt = battery_level
+            if battery_level - 10 < 0:
+                battery_level = 0
+            elif battery_level == 0:
+                requestRecharge(name, battery_level, choice)
+            else:
+                battery_level -= 10
+            print(f"{name}'s level of the battery was {prev_batt}. Now it is {battery_level}.")
+            prev_bore = boredom
+            boredom += 5
+            print(f"{name}'s level of boredom was {prev_bore}. Now it is {boredom}.")
+            print(f"{name} has become smarter!")
+            continue
+        elif choice == choices[6]:
+            if rust == 0:
+                print(f"{name} is fine, no need to oil!")
+            else:
+                prev_rust = rust
+                rust -= 20
+                if rust < 0:
+                    rust = 0
+                print(f"{name}'s level of rust was {prev_rust}. Now it is {rust}. {name} is less rusty!")
+            continue
+        elif choice == choices[7]:
+            prev_rust = rust
+            rust = unfortunateEvent(name, rust)
+            if skill < 50:
+                print(f"{name} has got to learn before working!")
+            else:
+                p_batt = battery_level
+                p_heat = over_heat
+                p_boredom = boredom
+                if battery_level - 10 < 0:
+                    battery_level = 0
+                else:
+                    battery_level -= 10
+                    boredom += 10
+                    over_heat += 10
+
+                print(f"{name}'s level of boredom was {p_boredom}. Now it is {boredom}.")
+                print(f"{name}'s level of overheat was {p_heat}. Now it is {over_heat}.")
+                print(f"{name}'s level of the battery was {p_batt}. Now it is {battery_level}.")
+                print(f"{name}'s level of rust was {prev_rust}. Now it is {rust}.")
+                print(f"{name} did well!")
+
+            continue
+        elif choice == choices[8]:
             break
-        print("Game over")
+
+    print("Game over")
 
 
 controlRobot()
